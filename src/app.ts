60d652c95692d3d01e10a5082234f8e7493da9ae
unknown <alif.ashrafulislam@gmail.com>
2023-06-13 11:03:42 +0600

    [Done] trycatch pattern and sendResponse pattern in shared folder

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { UserRouter } from './app/modules/user/user.route';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import router from './app/routes';
 const app: Application = express();
 
 app.use(cors());
@@ -12,8 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Application routes
-app.use('/api/v1/users/', UserRouter);
-app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
+// app.use('/api/v1/users/', UserRouter);
+// app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
+app.use('/api/v1/', router);
 
 //Testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
---
+++ src/app/routes/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { UserRouter } from '../modules/user/user.route';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+
+const router = express.Router();
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRouter,
+  },
+  {
+    path: '/academic-semesters/',
+    route: AcademicSemesterRoutes,
+  },
+];
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+
+export default router;
